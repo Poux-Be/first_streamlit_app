454e0f1148f3987271c2b8d1a3083b3ce7d93890
Poux-Be <louis.poux@bearingpoint.com>
2022-06-28 14:38:41 +0200

    breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,9 @@ import streamlit
  
 # ----- Main -----
 streamlit.title("My Parents New Healthy Diner")
+
+streamlit.header("Breakfast Menu")
+
+streamlit.text("Omelette aux herbes")
+streamlit.text("Omelette aux champignons")
+streamlit.text("Omelette au beurre")
